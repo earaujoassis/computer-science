59eddd360153bed93570f23c9fb32ad1c79565b9
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-05-10 01:34:18 -0300

    hotfix: fix python installation script

--- graduate-works/ufabc-symbolic-regression-project/scripts/install-python.sh
+++ graduate-works/ufabc-symbolic-regression-project/scripts/install-python.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get install libssl-dev openssl
+sudo apt-get update
+sudo apt-get install libssl-dev openssl build-essential
 wget https://www.python.org/ftp/python/3.12.10/Python-3.12.10.tgz
 tar xzvf Python-3.12.10.tgz
 cd Python-3.12.10
