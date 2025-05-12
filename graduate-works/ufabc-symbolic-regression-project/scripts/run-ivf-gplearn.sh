cef33e539c9254c1ed6dc87622a758665e558a3a
Carlos A. <588595+earaujoassis@general.noreply.quatrolabs.com>
2025-05-12 09:39:19 -0300

    chore: final results and final project setup

---
+++ graduate-works/ufabc-symbolic-regression-project/scripts/run-ivf-gplearn.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mkdir -p ./results
+mkdir -p ./results/ivf-gplearn
+
+for srsd_category in easy medium hard; do
+    RESULT_DIR=./results/ivf-gplearn/srsd-feynman_${srsd_category}
+    mkdir -p ${RESULT_DIR}
+    echo "[SRSD category: ${srsd_category}]"
+    for filepath in ./datasets/srsd-feynman_${srsd_category}/train/*; do
+        echo "[Current file: ${filepath}]"
+        PARENT_DIR=$(dirname $(dirname $filepath))
+        FILE_NAME=$(basename $filepath)
+        TRAIN_FILE=${PARENT_DIR}/train/${FILE_NAME}
+        VAL_FILE=${PARENT_DIR}/val/${FILE_NAME}
+        TEST_FILE=${PARENT_DIR}/test/${FILE_NAME}
+        python ./runners/solution_ivf_gplearn.py --config ./configs/config.ivf-gplearn.yaml --train ${TRAIN_FILE} --val ${VAL_FILE} --test ${TEST_FILE} --out ${RESULT_DIR}/${FILE_NAME}.csv
+    done
+done
